786ea02886dd8e05f876a4b04ca6ff8a60964e24
Armagetto <forfxp12@gmail.com>
2022-08-15 14:27:43 +0300

    Backup before change

--- Project11/Header.h
+++ Project11/Header.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 #define _CRT_SECURE_NO_WARNINGS
-#define BOARD_SIZE 8 //8X8 is for chess
+#define BOARD_SIZE 9 //8X8 is for chess + 1 for numbers and latters
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
@@ -10,6 +10,7 @@
 enum PieceType { empty, pawn = 1, bishop, knight, rook=5, queen = 9, king};
 enum playerType { black, white, nor};
 enum boardCoordinates { A , B, C, D, E, F, G, H};
+
 
 /*structs*/
 typedef struct
@@ -26,11 +27,21 @@ typedef struct
 
 
 /*functions*/
+
+//colors (windows)
+void colorChange_white() { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7); };
+void colorChange_gray() { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8); };
+void colorChange_whiteBlue() { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 23); };
+
+
+//main functions
 void BuildChessBoard(board* b)
 {
 	//sets all pieces to empty
-	for (int counter1 = 0; counter1 < BOARD_SIZE; counter1++) {
-		for (int counter2 = 0; counter2 < BOARD_SIZE; counter2++) {
+	for (int counter1 = 0; counter1 < BOARD_SIZE; counter1++)
+	{
+		for (int counter2 = 0; counter2 < BOARD_SIZE; counter2++)
+		{
 			b->piecesArray[counter1][counter2].piceType = empty;
 			b->piecesArray[counter1][counter2].playerType = nor;
 		}
@@ -49,7 +60,8 @@ void BuildChessBoard(board* b)
 	b->piecesArray[D][7].piceType = queen;
 	b->piecesArray[E][7].piceType = king;
 
-	for (int i = 0; i < BOARD_SIZE;i++) {
+	for (int i = 0; i < BOARD_SIZE-1;i++)
+	{
 		b->piecesArray[i][6].piceType = pawn;
 		b->piecesArray[i][6].playerType = black;
 		b->piecesArray[i][7].playerType = black;
@@ -68,7 +80,8 @@ void BuildChessBoard(board* b)
 	b->piecesArray[D][0].piceType = queen;
 	b->piecesArray[E][0].piceType = king;
 
-	for (int i = 0; i < BOARD_SIZE; i++) {
+	for (int i = 0; i < BOARD_SIZE-1; i++)
+	{
 		b->piecesArray[i][1].piceType = pawn;
 		b->piecesArray[i][1].playerType = white;
 		b->piecesArray[i][0].playerType = white;
@@ -78,61 +91,82 @@ void BuildChessBoard(board* b)
 
 void printBoard(board* b)
 {
+	//vars
+	char lineAlphabet[9] = { 'A','B','C','D','E','F','G','H',' '};
+	char lineNumbers[8] = { '8','7','6','5','4','3','2','1'};
+	int blockColorDecider = white;
 
-	for (int x = 0; x < BOARD_SIZE;x++) {
-		for (int y = 0; y < BOARD_SIZE; y++) {
+	//print
+	for (int x = 0; x < BOARD_SIZE; x++)
+	{
+		for (int y = 0; y < BOARD_SIZE; y++)
+		{
+			//print first '|' in the correct color
+
+			//printf("");
+
 			switch (b->piecesArray[y][x].piceType)
 			{
 			case pawn:
-				if (!b->piecesArray[y][x].playerType)
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+				if (b->piecesArray[y][x].playerType)
+					colorChange_gray();
 				else
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+					colorChange_white();
 					printf("|P|");
 				break;
 			case rook:
 				if (b->piecesArray[y][x].playerType)
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+					colorChange_gray();
 				else
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+					colorChange_white();
 				printf("|R|");
 				break;
 			case knight:
 				if (b->piecesArray[y][x].playerType)
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+					colorChange_gray();
 				else
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+					colorChange_white();
 				printf("|N|");
 				break;
 			case bishop:
 				if (b->piecesArray[y][x].playerType)
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+					colorChange_gray();
 				else
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+					colorChange_white();
 				printf("|B|");
 				break;
 			case king:
 				if (b->piecesArray[y][x].playerType)
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+					colorChange_gray();
 				else
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+					colorChange_white();
 				printf("|K|");
 				break;
 			case queen:
 				if (b->piecesArray[y][x].playerType)
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+					colorChange_gray();
 				else
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+					colorChange_white();
 				printf("|Q|");
 				break;
 			case empty:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
-				printf("|_|");
+				colorChange_whiteBlue();
+				if (x == BOARD_SIZE-1) 
+				{
+					printf(" %c ",lineAlphabet[y]);
+				}
+				else if (y == BOARD_SIZE - 1) {
+					printf(" %c ", lineNumbers[x]);
+				}
+				else {
+					colorChange_white();
+					printf("|_|");
+				}
+					
 			default:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+				colorChange_gray();
 				break;
 			}
-
 			if (y == BOARD_SIZE - 1) {
 				printf("\n");
 			}
